d641505614c3ba8f3435fa274a739d89264e8251
shifa7699 <shifaiqbal790815@gmail.com>
2026-05-31 11:22:59 +0530

    AgroVision - AI Powered Crop Disease Detection

--- crop_disease_detector/test.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-import streamlit as st
-import numpy as np
-from PIL import Image
-
-print("TensorFlow:", tf.__version__)
-print("Streamlit:", st.__version__)
-print("All packages OK! Ready to build.")
